cc9fd05a1e685208939dbd4beb23868d8086a44f
GaspalCatalin <gaspalcatalin@gmail.com>
2024-12-19 23:19:26 +0000

    Added HTML and JavaScript files
    Added HTML file with the first JavaScript external script.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,31 @@
+window.alert("Hello, World!"); //Creating the first alert
+document.write("Hello, World!"); //Creating the first print in JavaScript
+
+var A="This is a string"; //Defining the first variables in JavaScript
+document.write(A);
+
+var B="This is a string variable alert"; //Defining and printing the first string variable alert
+window.alert(B);
+
+document.write("<br>", "\"What do you want for the Christmas dinner, darling?\", asked Martin."); //Using the escape characters
+
+document.write("<br>")
+document.write("\“The principal office of history I take to be this:" //Using the concatenated string
+    +" to prevent virtuous actions from being forgotten," 
+    +" and that evil words and deeds should fear an infamous reputation with posterity.\”" 
+    +" - Tacitus, Roman historian");
+
+var C="Concatenated" + "String"; //Defining and printing a contatenated string variable
+document.write("<br>")
+document.write(C)
+
+var Sent1="This is the beginning of the string", Sent2=" and this is the end of the string."; //Defining two variables, concatenate and print them
+document.write("<br>")
+document.write(Sent1+Sent2)
+
+var Family="Bubu", Dad="Johny", Mom="Mary", Daughter="Boh", Son="Puf"; //Creating multiple variables
+document.write("<br>")
+document.write(Son);                                                   //in one statement and print one variable
+
+document.write("<br>")
+document.write(3*3); //Practising: statement with an expression
